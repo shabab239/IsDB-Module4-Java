28a5f262073d11e4f654fcbd198f8e26fc8a001a
Shabab Ahmed <sabab239.1@gmail.com>
2024-04-21 18:52:59 +0600

    21.04.2024

---
+++ NetBeansProjects/Class1-(21.04.2024)/src/class1/Class1.java
@@ -0,0 +1,13 @@
+package class1;
+
+/**
+ *
+ * @author Shabab (1281539)
+ */
+public class Class1 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ NetBeansProjects/Test Project/src/test/project/TestProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test.project;
+
+/**
+ *
+ * @author Shabab (1281539)
+ */
+public class TestProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World for the nTH time!");
+        
+    }
+    
+}
