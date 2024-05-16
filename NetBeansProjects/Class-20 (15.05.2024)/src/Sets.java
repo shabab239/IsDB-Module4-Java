d961a71cc9659fb9df64e0635ab9439d3840c010
Shabab Ahmed <52327464+shabab239@users.noreply.github.com>
2024-05-16 18:55:12 +0600

    16.05.2024

--- NetBeansProjects/Class-20 (15.05.2024)/src/Sets.java
+++ NetBeansProjects/Class-20 (15.05.2024)/src/Sets.java
@@ -1,8 +1,10 @@
 
+import academy.Student;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
+import java.util.TreeSet;
 
 /**
  *
@@ -16,17 +18,31 @@ public class Sets {
         stringSet.add("LMAO");
         stringSet.add("LOL");
         stringSet.add("LOL");
-        
+
         System.out.println(stringSet);
 
         Set<String> stringLinkedSet = new LinkedHashSet<>(); //sorted //default initial capacity - 16, load factor 0.75 (ranges from 0 to 1)
         stringLinkedSet.add("LOL");
         stringLinkedSet.add("LMAO");
         stringLinkedSet.add("LOL"); //this wont be in the hashset
-        
+
         System.out.println(stringLinkedSet);
-       
+
+        TreeSet<Student> treeSet = new TreeSet<>(new Comparator<Student>() { //TreeSet doesn’t allow even a single null element
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }); //sorted set
         
+        treeSet.add(new Student("Shabab"));
+        treeSet.add(new Student("Adnan"));
+        treeSet.add(new Student("Luban"));
+
+        for (Student s : treeSet) {
+            System.out.println(s.getName());
+        }
+
     }
 
 }
---
+++ NetBeansProjects/Class-20 (15.05.2024)/src/academy/Student.java
@@ -0,0 +1,34 @@
+package academy;
+
+/**
+ *
+ * @author Shabab-1281539
+ */
+public class Student {
+
+    private Integer roll;
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+    
+    
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
