d0e332d1ae9842d49576d1037ff89f940b0fbf50
Shabab Ahmed <sabab239.1@gmail.com>
2024-05-29 04:23:57 +0600

    JEE59Store

---
+++ NetBeansProjects/JEE59Store/src/Main.java
@@ -0,0 +1,15 @@
+
+import store.JEE59Store;
+
+
+/**
+ *
+ * @author shaba
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new JEE59Store().setVisible(true);
+    }
+    
+}
