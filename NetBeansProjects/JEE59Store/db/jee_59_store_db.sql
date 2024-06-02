53e479b64b91be52a4589525b8051399ae5762b4
Shabab Ahmed <52327464+shabab239@users.noreply.github.com>
2024-06-02 18:52:33 +0600

    02.06.2024

--- NetBeansProjects/JEE59Store/db/jee_59_store_db.sql
+++ NetBeansProjects/JEE59Store/db/jee_59_store_db.sql
@@ -1,49 +1,107 @@
-/*
-SQLyog Professional v13.1.1 (64 bit)
-MySQL - 10.4.11-MariaDB : Database - jee_59_store
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jee_59_store
+-- ------------------------------------------------------
+-- Server version	8.0.37
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`jee_59_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `jee_59_store`;
-
-/*Table structure for table `product` */
+--
+-- Table structure for table `product`
+--
 
 DROP TABLE IF EXISTS `product`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
   `unitPrice` double DEFAULT NULL,
-  `quantity` int(11) NOT NULL,
   `salesPrice` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `product` */
+--
+-- Dumping data for table `product`
+--
 
-insert  into `product`(`id`,`name`,`unitPrice`,`quantity`,`salesPrice`) values 
-(14,'Apples',80,100,120),
-(15,'Bananas',40,150,70),
-(16,'Milk',25,50,50),
-(17,'Bread',20,80,40),
-(18,'Eggs',15,120,30),
-(19,'Chicken',400,60,600),
-(20,'Potatoes',50,200,80),
-(21,'Tomatoes',120,90,200),
-(22,'Rice',70,110,100),
-(23,'Pasta',40,70,60);
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Rice',60,70),(2,'Milk',45,50),(3,'Egg',10,12),(4,'Bread',25,30),(5,'Chicken',180,200),(6,'Tomatoe',40,45),(7,'Potatoe',30,35),(8,'Onion',25,28),(9,'Apple',80,90),(10,'Mango',60,70),(11,'Banana',35,40);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sales` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `salesPrice` double NOT NULL,
+  `quantity` int NOT NULL,
+  `totalPrice` double NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales`
+--
+
+LOCK TABLES `sales` WRITE;
+/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (1,'Apple',40,2,80,'2024-06-02'),(2,'Mango',35,7,245,'2024-06-02');
+/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `quantity` int NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock`
+--
+
+LOCK TABLES `stock` WRITE;
+/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+INSERT INTO `stock` VALUES (1,'Apple',28,'2024-06-02'),(2,'Mango',5,'2024-06-02');
+/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-02 18:51:44
